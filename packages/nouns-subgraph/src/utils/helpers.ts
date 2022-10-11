ce2ba60938cd235f47d4e2d682ecc113026f65da
yanuar-ar <yanuar.ar@gmail.com>
2022-10-11 10:02:02 +0700

    revert subgraph to NounsDAOLogicV1

--- packages/nouns-subgraph/src/utils/helpers.ts
+++ packages/nouns-subgraph/src/utils/helpers.ts
@@ -1,11 +1,4 @@
-import {
-  Account,
-  Delegate,
-  Proposal,
-  Governance,
-  Vote,
-  DynamicQuorumParams,
-} from '../types/schema';
+import { Account, Delegate, Proposal, Governance, Vote } from '../types/schema';
 import { ZERO_ADDRESS, BIGINT_ZERO, BIGINT_ONE } from './constants';
 
 export function getOrCreateAccount(
@@ -31,34 +24,32 @@ export function getOrCreateAccount(
   return tokenHolder as Account;
 }
 
-// These two functions are split up to minimize the extra code required
-// to handle return types with `Type | null`
-export function getOrCreateDelegate(id: string): Delegate {
-  return getOrCreateDelegateWithNullOption(id, true, true) as Delegate;
-}
-
-export function getOrCreateDelegateWithNullOption(
+export function getOrCreateDelegate(
   id: string,
   createIfNotFound: boolean = true,
   save: boolean = true,
-): Delegate | null {
+): Delegate {
   let delegate = Delegate.load(id);
+
   if (delegate == null && createIfNotFound) {
     delegate = new Delegate(id);
     delegate.delegatedVotesRaw = BIGINT_ZERO;
     delegate.delegatedVotes = BIGINT_ZERO;
     delegate.tokenHoldersRepresentedAmount = 0;
     delegate.nounsRepresented = [];
+
     if (id != ZERO_ADDRESS) {
       let governance = getGovernanceEntity();
-      governance.totalDelegates = governance.totalDelegates.plus(BIGINT_ONE);
+      governance.totalDelegates = governance.totalDelegates + BIGINT_ONE;
       governance.save();
     }
+
     if (save) {
       delegate.save();
     }
   }
-  return delegate;
+
+  return delegate as Delegate;
 }
 
 export function getOrCreateVote(
@@ -91,7 +82,7 @@ export function getOrCreateProposal(
 
     let governance = getGovernanceEntity();
 
-    governance.proposals = governance.proposals.plus(BIGINT_ONE);
+    governance.proposals = governance.proposals + BIGINT_ONE;
     governance.save();
 
     if (save) {
@@ -118,19 +109,4 @@ export function getGovernanceEntity(): Governance {
   }
 
   return governance as Governance;
-}
-
-export function getOrCreateDynamicQuorumParams(): DynamicQuorumParams {
-  let params = DynamicQuorumParams.load('LATEST');
-
-  if (params == null) {
-    params = new DynamicQuorumParams('LATEST');
-    params.minQuorumVotesBPS = 0;
-    params.maxQuorumVotesBPS = 0;
-    params.quorumCoefficient = BIGINT_ZERO;
-
-    params.save();
-  }
-
-  return params as DynamicQuorumParams;
 }
